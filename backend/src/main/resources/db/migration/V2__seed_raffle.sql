158429bc7e287d1e206e35917cbc554f8b35f877
Hilberto Fernandes <betogouveia1@outlook.com>
2026-09-02 11:21:19 -0300

    fix: update draw date for Chá Rifa do José Lucca

--- backend/src/main/resources/db/migration/V2__seed_raffle.sql
+++ backend/src/main/resources/db/migration/V2__seed_raffle.sql
@@ -1,5 +1,5 @@
 insert into raffles (id, title, draw_at, number_price, first_prize, second_prize, pix_key)
-values ('9d325c50-d845-4bf3-85e7-1bf14e9ed928', 'Chá Rifa do José Lucca', '2026-10-03 22:30:00+00', 25.00, 250.00, 100.00, null);
+values ('9d325c50-d845-4bf3-85e7-1bf14e9ed928', 'Chá Rifa do José Lucca', '2026-10-10 22:30:00+00', 25.00, 250.00, 100.00, null);
 
 insert into raffle_numbers (id, raffle_id, number, status)
 select gen_random_uuid(), '9d325c50-d845-4bf3-85e7-1bf14e9ed928', value, 'AVAILABLE'
